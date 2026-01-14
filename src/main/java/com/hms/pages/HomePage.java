3a8bc89bd831e9e89fb1dd6069d2c544c5b840bc
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2026-01-14 18:49:00 +0530

    Implement HomePage class with navigation methods using locators

--- src/main/java/com/hms/pages/HomePage.java
+++ src/main/java/com/hms/pages/HomePage.java
@@ -1,5 +1,30 @@
 package com.hms.pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
+public class HomePage {
+	WebDriver driver;
+
+	// Locators for Menu Items
+	private By logoutLink = By.id("lbtnLogOut");
+	private By bloodsMenu = By.xpath("//*[@id=\"NavigationMenu\"]/ul/li[2]/a");
+	private By banksOption = By.xpath("//*[@id=\"NavigationMenu:submenu:3\"]/li[1]/a");
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void clickLogout() {
+		driver.findElement(logoutLink).click();
+	}
+
+	// Perform Hover over blood and then click the banks option
+	public void navigateToBloodBanks() throws InterruptedException {
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(bloodsMenu)).perform();
+		Thread.sleep(1000);
+		driver.findElement(banksOption).click();
+	}
 }
